3415c633890bb6d05f443832632d79705af54501
Pratham Dindorkar <82752264+PrathamDindorkar@users.noreply.github.com>
2023-07-10 12:36:09 +0530

    Add files via upload

---
+++ Source/StreetMapRuntime/StreetMap.cpp
@@ -0,0 +1,25 @@
+#include "StreetMap.h"
+#include "EditorFramework/AssetImportData.h"
+
+UStreetMap::UStreetMap()
+{
+#if WITH_EDITORONLY_DATA
+	if( !HasAnyFlags( RF_ClassDefaultObject ) )
+	{
+		AssetImportData = NewObject<UAssetImportData>( this, TEXT( "AssetImportData" ) );
+	}
+#endif
+}
+
+
+void UStreetMap::GetAssetRegistryTags( TArray<FAssetRegistryTag>& OutTags ) const
+{
+#if WITH_EDITORONLY_DATA
+	if( AssetImportData )
+	{
+		OutTags.Add( FAssetRegistryTag( SourceFileTagName(), AssetImportData->GetSourceData().ToJson(), FAssetRegistryTag::TT_Hidden ) );
+	}
+#endif
+
+	Super::GetAssetRegistryTags( OutTags );
+}
